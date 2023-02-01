7303c42efb8d70658ef3236935dcb60586140254
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2023-02-01 12:16:28 +0100

    :tada: Starter commit

---
+++ src/main/java/com/example/workbookgraphql/WorkbookGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.workbookgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbookGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkbookGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workbookgraphql/WorkbookGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workbookgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbookGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
